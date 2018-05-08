064a3787f33cbcabb3252d4a3e9a1263dd1118bd
Superbiael <39025166+Superbiael@users.noreply.github.com>
2018-05-08 16:28:07 -0700

    Create sougohelp.js

---
+++ commands/sougohelp.js
@@ -0,0 +1,16 @@
+const Discord = require("discord.js");
+
+module.exports.run = async (bot,message, args) => {
+
+  let guideembed = new Discord.RichEmbed()
+  .setColor("#825aae")
+  .addField("s!commands", "mafia (alias:maf) ||  c - ss rank || smooch ")
+  .addField("Other commands", "I love you Sougo || I would die for you sougo || Good morning Sougo || Good night Sougo")
+  .addField("8ball command", "s!sougo <question>");
+
+  message.channel.send(guideembed);
+}
+
+module.exports.help = {
+  name: "sougohelp"
+}
