f7d5738e4feae6c248e50bddb4e037f7c39e72d5
Superbiael <39025166+Superbiael@users.noreply.github.com>
2018-05-09 14:02:29 -0700

    Update sougo.js

--- commands/sougo.js
+++ commands/sougo.js
@@ -2,7 +2,7 @@ const Discord = require("discord.js");
 
 module.exports.run = async (bot, message, args) => {
    if(!args[1]) return message.reply("I never learned how to read.");
-   let replies = ["Um, I think it's better if you ask Yamato-san..?", "That's no good, you know.", "I strongly advise you to do so. I'll be cheering you on.", "I'm sorry, I don't think I can help with that.", "Fufu, why not go for it?", "Do your best.","Absolutely not.", "I don't think so"];
+   let replies = ["Of course, I'm a big fan.", "Um, I think it's better if you ask Yamato-san..?", "That's no good, you know.", "I strongly advise you to do so. I'll be cheering you on.", "I'm sorry, I don't think I can help with that.", "Fufu, why not go for it?", "Do your best.","Absolutely not.", "I don't think so"];
 
    let result = Math.floor((Math.random() * replies.length));
    let question = args.slice(0).join(" ");
