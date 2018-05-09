5a2bc40cdbc3b219abad7cc51be4efd3b85ea8fc
Superbiael <39025166+Superbiael@users.noreply.github.com>
2018-05-08 20:29:04 -0700

    Update sougohelp.js

--- commands/sougohelp.js
+++ commands/sougohelp.js
@@ -8,7 +8,7 @@ module.exports.run = async (bot,message, args) => {
   .addField("s!commands:", "mafia (alias:maf) ||  c - ss rank || smooch ")
   .addField("Other commands:", "I love you Sougo || I would die for you sougo || Good morning Sougo || Good night Sougo")
   .addField("8ball command:", "s!sougo <question>")
-  .addField("Send DM:", "s!send <@user> <message>");
+  .addField("Send DM:", "s!send <@user> <message> || currently disabled");
 
   message.channel.send(guideembed);
 }
