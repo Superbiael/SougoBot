e724769a045465a42f70ffedb2bd8e1c359b4924
Superbiael <39025166+Superbiael@users.noreply.github.com>
2018-05-08 20:28:02 -0700

    Update sougohelp.js

--- commands/sougohelp.js
+++ commands/sougohelp.js
@@ -5,9 +5,10 @@ module.exports.run = async (bot,message, args) => {
   let guideembed = new Discord.RichEmbed()
   .setDescription("Do not include < > when using commands.")
   .setColor("#825aae")
-  .addField("s!commands", "mafia (alias:maf) ||  c - ss rank || smooch ")
-  .addField("Other commands", "I love you Sougo || I would die for you sougo || Good morning Sougo || Good night Sougo")
-  .addField("8ball command", "s!sougo <question>");
+  .addField("s!commands:", "mafia (alias:maf) ||  c - ss rank || smooch ")
+  .addField("Other commands:", "I love you Sougo || I would die for you sougo || Good morning Sougo || Good night Sougo")
+  .addField("8ball command:", "s!sougo <question>")
+  .addField("Send DM:", "s!send <@user> <message>");
 
   message.channel.send(guideembed);
 }
