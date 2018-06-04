0f60c1260f99731bafb5490ed7c046d000aeac36
Superbiael <39025166+Superbiael@users.noreply.github.com>
2018-06-03 20:00:33 -0700

    Update index.js

--- index.js
+++ index.js
@@ -34,8 +34,8 @@ bot.on('message', message => {
 
      msg = message.content.toLowerCase();
  
-       if(msg.startsWith (prefix + "quote")) {
-       number = 5;
+     if(msg.startsWith (prefix + "quote")) {
+       number = 8;
        var random = Math.floor (Math.random() * (number)) + 1;
        switch (random) {
          case 1: message.channel.send ("I wonder if you’ll watch over me from by my side."); break;
@@ -43,6 +43,9 @@ bot.on('message', message => {
          case 3: message.channel.send ("I want to become a little bit more manly."); break;
          case 4: message.channel.send ("While you were busy fucking around, I studied the blade."); break;
          case 5: message.channel.send ("I'll have Yamato-san and Mitsuki-san educate me on how to be sexy again."); break;
+         case 6: message.channel.send ("I'm seeing the word *HELL* all around me."); break;
+         case 7: message.channel.send ("This is so nice. I hope we'll be able to keep going like this. Since I used to collect TRIGGER's merchandise a lot, seeing people wearing ours made me feel nostalgic."); break;
+         case 8: message.channel.send ("I wonder if it's alright for me to be this happy..."); break;
       }
     }
   
