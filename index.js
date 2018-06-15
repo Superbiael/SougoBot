5a4befcef1243311d2075961f13a44a6844b08f5
Superbiael <39025166+Superbiael@users.noreply.github.com>
2018-06-15 16:46:08 -0700

    Update index.js

--- index.js
+++ index.js
@@ -25,7 +25,6 @@ fs.readdir("./commands/", (err, files) => {
 bot.on("ready", async () => {
   console.log(`${bot.user.username} is online on ${bot.guilds.size} servers!`);
   bot.user.setActivity("Oh No", {type:0});
-
 });
 
 bot.on('message', message => {
